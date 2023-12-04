9e1a25b51c89ab3bfe9ab60edf3ae260da2b18d8
Dave Jones <46875849+DavidFJones@users.noreply.github.com>
2023-12-03 21:01:36 -0700

    Player Controller 2
    TO DO:
    Make Pickup interactions work
    
    Clamp hook rotation when holding items
    
    Smooth grapple interaction
    
    Overhaul y velocity calculations
    
    Fix timing issues for hook / grappling based on distance
    
    Fix hook offset (that I somehow broke?...)

--- Scripts/Character Control/Modes/ClawMode.gd
+++ Scripts/Character Control/Modes/ClawMode.gd
@@ -1,8 +1,6 @@
-extends Node2D
+extends Area2D
 
 var state_active = true
-
-var shooting_claw = false
 
 @export var player_controller : Node
 
@@ -14,9 +12,13 @@ var held_item : Node
 enum Claw_State {DEFAULT, FIRING, GRAPPLED, HOLDING, TETHERING}
 var current_state = Claw_State.DEFAULT
 
+var target_collider
+
+var grapple_hold_position
+
 @export var min_firing_distance := 100.0
 
-@export var firing_speed := 600.0
+@export var firing_speed := 1.5
 
 var t = 0.0
 var starting_claw_position = Vector2()
@@ -24,6 +26,7 @@ var target_claw_position = Vector2()
 
 var grapple_target = Vector2()
 var starting_grapple_position = Vector2()
+
 
 func _process(delta):
 	if not state_active:
@@ -33,39 +36,45 @@ func _process(delta):
 		held_item.position = to_global(item_hold_position.position)
 		print(self.global_rotation_degrees)
 
-	if current_state == Claw_State.TETHERING:
-		print("wer flying")
+	if current_state == Claw_State.FIRING:
+		t += delta * 0.4 * firing_speed
+		self.global_position = starting_claw_position.lerp(target_claw_position, t)
+	elif current_state == Claw_State.TETHERING:
 		t += delta * 0.4
-		player_controller.position = starting_grapple_position.lerp(grapple_target, t)
-
-	queue_redraw()
+		player_controller.global_position = starting_grapple_position.lerp(grapple_target, t)
+		if player_controller.global_position.distance_to(grapple_target) <= 75:
+			hit_grapple_point()
+	elif current_state == Claw_State.GRAPPLED:
+		player_controller.global_position = grapple_hold_position
+	else:
+		self.global_position = get_parent().global_position
+		self.global_rotation = get_parent().global_rotation
+		queue_redraw()
 
 	if Input.is_action_just_pressed("input_fire"):
 		match current_state:
 			Claw_State.DEFAULT:
 				shoot_claw()
-			Claw_State.FIRING:
+			Claw_State.FIRING, Claw_State.TETHERING:
 				cancel_claw()
 			Claw_State.GRAPPLED:
 				release_grapple()
 			Claw_State.HOLDING:
 				throw_held_item()
 
-
 func shoot_claw():
 	if position.distance_to(get_parent().hit_point) >= min_firing_distance:
+		t = 0
 		current_state = Claw_State.FIRING
-		starting_claw_position = self.position
-		target_claw_position = get_parent().hit_point
-		shooting_claw = true		
-		
-		#for testing lets just hit what ever we look at
-		target_hit()
+		starting_claw_position = self.global_position
+		target_claw_position = to_global(get_parent().hit_point)
+		if get_parent().get_collider():
+			target_collider = get_parent().get_collider()
 
 func target_hit():
-	if get_parent().get_collider().has_method("pickup"):
+	if target_collider.has_method("pickup"):
 		grab_item()
-	elif get_parent().get_collider().has_method("grapple"):
+	elif target_collider.has_method("grapple"):
 		teathering()
 	else:
 		cancel_claw()
@@ -76,13 +85,19 @@ func grab_item():
 	current_state = Claw_State.HOLDING	
 	holding_item = true
 
+func hit_grapple_point():
+	current_state = Claw_State.GRAPPLED	
+	grapple_hold_position = player_controller.global_position
+
 func teathering():
+	t = 0
 	current_state = Claw_State.TETHERING	
 	starting_grapple_position = player_controller.global_position
-	grapple_target = to_global(get_parent().hit_point)
+	var offset = starting_grapple_position - to_global(get_parent().hit_point)
+	grapple_target = player_controller.global_position - offset * 0.5
 
 func cancel_claw():
-	print("claw canceled")
+	self.global_position = get_parent().global_position
 	current_state = Claw_State.DEFAULT	
 
 func throw_held_item():
@@ -97,8 +112,8 @@ func release_grapple():
 func _draw():
 	#I don't like this solution of redrawing every frame just to clear
 	#But I'm not sure how to clear everything in _draw, so instead we just draw an "empty" image when not needed
-	if get_parent().is_colliding():
-		if position.distance_to(get_parent().hit_point) >= min_firing_distance:
+	if get_parent().is_colliding() and current_state != Claw_State.FIRING:
+		if global_position.distance_to(get_parent().get_collision_point()) >= min_firing_distance:
 			if get_parent().get_collider().has_method("pickup"):
 				draw_circle(get_parent().hit_point, 10, Color(0, 255, 0))
 			elif get_parent().get_collider().has_method("grapple"):
@@ -110,4 +125,8 @@ func _draw():
 		return
 
 	#ugly way of just removing the drawn vector if no previous conditions are met
-	draw_circle(Vector2(), 0, Color(255, 255, 0))
+	draw_circle(Vector2(), 0, Color(0, 0, 0))
+
+func _on_body_entered(body):
+	if body == target_collider:
+		target_hit()
